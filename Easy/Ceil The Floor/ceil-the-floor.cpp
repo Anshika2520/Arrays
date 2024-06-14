d869a071c11dd425b8a56cbc236cf7e1b06a7cb6
Anshika2520 <121687073+Anshika2520@users.noreply.github.com>
2024-06-14 17:31:00 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Ceil The Floor/ceil-the-floor.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x);
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        auto ans = getFloorAndCeil(arr, n, x);
+        cout << ans.first << " " << ans.second << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+    sort(arr,arr+n);
+    int s=0;
+    int e=n-1;
+    int f=-1;
+    int c=-1;
+    int mid=0;
+    while(s<=e){
+     mid=s+(e-s)/2;
+        if(arr[mid]>=x){
+            c=arr[mid];
+            e=mid-1;
+        }
+        else{
+            s=mid+1;
+        }
+    }
+    s=0;e=n-1;
+    while(s<=e){
+        mid=s+(e-s)/2;
+        if(arr[mid]<=x){
+            f=arr[mid];
+            s=mid+1;
+        }
+        else{
+            e=mid-1;
+        }
+    }
+    return {f,c};
+}
