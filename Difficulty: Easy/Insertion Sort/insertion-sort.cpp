3740196d452c17b826d0be742e3ac81458862077
Anshika2520 <121687073+Anshika2520@users.noreply.github.com>
2024-07-31 12:01:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insertion Sort/insertion-sort.cpp
+++ Difficulty: Easy/Insertion Sort/insertion-sort.cpp
@@ -27,12 +27,18 @@ class Solution
     {
         for(int i=0;i<n;i++){
             int j=i;
-            while(j>0 && arr[j-1]>arr[j]){
+            while(j>0 && j<=n-1){
+                if(arr[j-1]>arr[j]){
                 int temp=arr[j];
                 arr[j]=arr[j-1];
                 arr[j-1]=temp;
                 j--;
             }
+            else{
+                j++;
+            }
+            }
+            
         }
     }
 };
